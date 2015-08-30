8aa475539e8aa6c11b700afbb65a53d2c6b0de35
hkgumbs <h.kofigumbs@gmail.com>
2015-08-30 16:49:32 -0500

    Write simple internal dependency script

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :main ^:skip-aot tictactoe-server.core
   :target-path "target/%s"
+  :java-source-paths ["jcheckouts/tictactoe-java/src/me/hkgumbs/tictactoe/main"]
   :plugins [[speclj "3.3.0"]]
   :profiles {:dev {:dependencies [[speclj "3.3.0"]]}
              :uberjar {:aot :all}})
