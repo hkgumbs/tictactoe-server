410b5b1723f8ec32cecf6a70944a073a1e5b5400
hkgumbs <h.kofigumbs@gmail.com>
2015-09-01 14:44:39 -0500

    Use webserver.core/main logic to run server

--- src/tictactoe_server/core.clj
+++ src/tictactoe_server/core.clj
@@ -1,7 +1,14 @@
 (ns tictactoe-server.core
-  (:gen-class))
+  (:gen-class)
+  (:require [webserver.app :as app]
+            [tictactoe-server.app :as backing-app]
+            [tictactoe-server.new]
+            [tictactoe-server.move]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main [& args]
+  (let [server (java.net.ServerSocket. 5000)
+        {:keys [valid-request-handler]} backing-app/responder]
+    (println "Serving Tic Tac Toe over HTTP...")
+    (while (not (.isClosed server))
+      (let [socket (.accept server)]
+        (app/relay valid-request-handler socket)))))
