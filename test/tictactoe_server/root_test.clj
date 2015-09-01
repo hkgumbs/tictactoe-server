f6a0c6cc1f4b6406aa4f45733955f5b86f09eef0
hkgumbs <h.kofigumbs@gmail.com>
2015-09-01 16:47:18 -0500

    Serve empty index.html from root

---
+++ test/tictactoe_server/root_test.clj
@@ -0,0 +1,10 @@
+(ns tictactoe-server.root-test
+  (:require [speclj.core :refer :all]
+            [tictactoe-server.root]
+            [tictactoe-server.mock-socket :as socket]))
+
+(describe "/"
+  (it "responds with HTML"
+    (let [response (socket/connect "/" "")]
+      (should (.startsWith response "HTTP/1.1 200 OK"))
+      (should-contain #"Content-Type:( )?text/html" response))))
