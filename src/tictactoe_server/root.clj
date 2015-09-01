1a778380bdfc85da30e082e8fa11dd0a97efc3f5
hkgumbs <h.kofigumbs@gmail.com>
2015-09-01 16:47:18 -0500

    Serve empty index.html from root

---
+++ src/tictactoe_server/root.clj
@@ -0,0 +1,10 @@
+(ns tictactoe-server.root
+  (:require [tictactoe-server.app :as app]
+            [webserver.response :as response]))
+
+(defmethod app/route "/" [_]
+  (let [html (slurp "assets/index.html")]
+    (str
+      (response/make
+        200 {:Content-Type "text/html" :Content-Length (count html)})
+      html)))
