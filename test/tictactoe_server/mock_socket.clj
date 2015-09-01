e5c9e297ad23b8c6d3097ebea39d2df57def2771
hkgumbs <h.kofigumbs@gmail.com>
2015-09-01 12:58:24 -0500

    Copy mock-socket/make from webserver-clojure
    Despite relying on the .jar file, I cannot require files
    from the webserver-clojure test directory. This is a
    makeshift solution until I think of something better.

--- test/tictactoe_server/mock_socket.clj
+++ test/tictactoe_server/mock_socket.clj
@@ -1,14 +1,20 @@
 (ns tictactoe-server.mock-socket
   (:require [speclj.core :refer :all]
-            [webserver.mock-socket :as mock-socket]
             [tictactoe-server.app :as app]
             [cheshire.core :as json]))
 
 (def handler (:valid-request-handler app/responder))
 (def template {:method "GET" :version "HTTP/1.1"})
 
+(defn- make [input]
+  (let [ouput-stream (java.io.ByteArrayOutputStream.)]
+    (proxy [java.net.Socket]
+      []
+      (getInputStream [] (java.io.ByteArrayInputStream. (.getBytes input)))
+      (getOutputStream [] ouput-stream))))
+
 (defn connect [uri parameters]
-  (let [socket (mock-socket/make "")
+  (let [socket (make "")
         request (into template {:uri uri :parameters parameters})]
     (handler socket request)
     (str (.getOutputStream socket))))
