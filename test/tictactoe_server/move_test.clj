9ce37bb1048890b70a693799f0c91c1fe139f0a8
hkgumbs <h.kofigumbs@gmail.com>
2015-09-03 17:32:42 -0500

    Add test for vs=minimax

--- test/tictactoe_server/move_test.clj
+++ test/tictactoe_server/move_test.clj
@@ -6,15 +6,26 @@
             [tictactoe-server.mock-socket :as socket])
   (:import [me.hkgumbs.tictactoe.main.java.board SquareBoard Board$Mark]))
 
-(describe "Request to /move"
-  (it "adds piece to board"
+(describe "Naive CPU"
+  (it "adds piece to board in first slot"
     (socket/connect "/new" "size=3&vs=naive")
     (socket/validate-body
       (socket/connect "/move" "position=4")
       {:board (-> (SquareBoard. 3)
                   (.add 4 Board$Mark/X)
-                  (.add 0 Board$Mark/O) .toString)}))
-  (it "400s on invalid input"
+                  (.add 0 Board$Mark/O) .toString)})))
+
+(describe "Minimax"
+  (it "adds piece at best slot"
+    (socket/connect "/new" "size=3&vs=minimax")
+    (socket/validate-body
+      (socket/connect "/move" "position=0")
+      {:board (-> (SquareBoard. 3)
+                  (.add 0 Board$Mark/X)
+                  (.add 4 Board$Mark/O) .toString)})))
+
+(describe "Invalid input to /move"
+  (it "400s"
     (socket/connect "/new" "size=3&vs=naive")
     (should=
       (response/make 400)
