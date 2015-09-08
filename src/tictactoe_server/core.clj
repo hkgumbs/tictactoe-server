c36a779c772739fc32cb560c07e43252d36a6025
hkgumbs <h.kofigumbs@gmail.com>
2015-09-08 09:11:55 -0500

    Fix argument syntax

--- src/tictactoe_server/core.clj
+++ src/tictactoe_server/core.clj
@@ -2,11 +2,12 @@
   (:gen-class)
   (:require [webserver.app :as app]
             [tictactoe-server.app :as backing-app]
+            [tictactoe-server.root]
             [tictactoe-server.new]
             [tictactoe-server.move]
             [tictactoe-server.util :as util]))
 
-(defn -main [[port]]
+(defn -main [& [port]]
   (let [server (java.net.ServerSocket. (util/parse-int port 5000))
         {:keys [valid-request-handler]} backing-app/responder]
     (println "Serving Tic Tac Toe over HTTP...")
