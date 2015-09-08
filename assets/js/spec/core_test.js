7ec816b0e9f45d03f98e033de82c934a385b8e5c
hkgumbs <h.kofigumbs@gmail.com>
2015-09-08 14:19:37 -0500

    Test button construction

--- assets/js/spec/core_test.js
+++ assets/js/spec/core_test.js
@@ -1,9 +1,32 @@
-describe("App", function() {
-    it("builds request correctly for /new", function() {
+describe('App', function() {
+    beforeEach(function() { jasmine.Ajax.install(); });
+    afterEach(function() { jasmine.Ajax.uninstall(); });
+
+    it('builds request correctly for /new', function() {
         var fixture =
             '<input type="number" name="size" value="4">' +
             '<select name="vs"><option value="minimax"></option></select>';
         setFixtures(fixture);
-        expect(getNewGameUri()).toBe("/new?size=4&vs=minimax");
+        expect(getNewGameUri()).toBe('new?size=4&vs=minimax');
     });
+
+    it('creates buttons based on JSON response', function() {
+        testButtonSetup(3);
+        expect($('.move').length).toBe(9);
+        testButtonSetup(5);
+        expect($('.move').length).toBe(25);
+    });
+
+    function testButtonSetup(size) {
+        setFixtures('<div class="game"></div>');
+        var board = "";
+        for (var i = 0; i < size * size; i++)
+            board += '-';
+
+        setupNewGame('/new?size=' + size + '&vs=minimax');
+        jasmine.Ajax.requests.mostRecent().respondWith({
+            'status': 200,
+            'responseText': '{"board": "' + board + '"}'
+        });
+    };
 });
--- assets/js/src/core.js
+++ assets/js/src/core.js
@@ -1,5 +1,24 @@
 function getNewGameUri() {
-    return "/new?" +
-        "size=" + $("[name=size]").val() + "&" +
-        "vs=" + $("[name=vs]").val();
+    return "new?" +
+        'size=' + $('[name=size]').val() + '&' +
+        'vs=' + $('[name=vs]').val();
 }
+
+function makeButtons(json) {
+    var board = json['board'];
+    var size = Math.sqrt(board.length);
+    var html = '';
+    for (var i = 0; i < size; i++) {
+        for (var j = 0; j < size; j++) {
+            html += '<button class="move">' + (i*3 + j) + '</button>';
+        }
+        html += '<br>';
+    }
+    $('.game').html(html);
+}
+
+function setupNewGame(uri) {
+    $.getJSON(uri, makeButtons);
+}
+
+$('[name=new]').on('click', function() { setupNewGame(getNewGameUri()); });
