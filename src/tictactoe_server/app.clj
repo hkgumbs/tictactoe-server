4fe324f79b5247b0a2cfd5ce292fdbaa7d3b8d92
hkgumbs <h.kofigumbs@gmail.com>
2015-09-17 13:42:22 -0500

    Fix run command

--- src/tictactoe_server/app.clj
+++ src/tictactoe_server/app.clj
@@ -5,7 +5,7 @@
             [clojure.java.io :as io]))
 
 (defmulti route :uri)
-(defmethod route :default [_])
+(defmethod route :default [_] :unmatched)
 (defmethod route "/" [_] (slurp "assets/index.html"))
 
 (defn map-parameters [request]
@@ -13,4 +13,5 @@
 
 (defn handle [socket request]
   (if-let [response (util/respond (route (map-parameters request)))]
-    (do (doseq [r response] (io/copy r (.getOutputStream socket))) true)))
+    (if-not (= response :unmatched)
+      (do (doseq [r response] (io/copy r (.getOutputStream socket))) true))))
--- src/tictactoe_server/core.clj
+++ src/tictactoe_server/core.clj
@@ -2,16 +2,17 @@
   (:gen-class)
   (:require [webserver.servlet :as servlet]
             [tictactoe-server.app :as app]
-            [tictactoe-server.root]
             [tictactoe-server.start]
             [tictactoe-server.move]
             [tictactoe-server.storage]
             [tictactoe-server.util :as util])
   (:import tictactoe_server.storage.AtomStorage))
 
+(def storage (AtomStorage. (atom {})))
 (defn handle-with-atom-storage [handler]
-  (fn [socket request]
-    (handler socket (assoc request :storage (AtomStorage. (atom {}))))))
+  (fn [socket request] (handler socket (assoc request :storage storage))))
 
 (defn -main [& args]
-  (servlet/start (concat ["-d" "assets"] args) handle-with-atom-storage))
+  (servlet/start
+    (concat ["-d" "assets"] args)
+    (handle-with-atom-storage app/handle)))
